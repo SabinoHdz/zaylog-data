cc3dad51f0d8d9b1fac251f6c05094945829db3d
Sabino Hernandez Paulino <sabino.hernandez@exos.mx>
2023-08-20 21:26:45 -0600

    implementacion de un pool

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,10 +1,16 @@
 const { Pool } = require('pg');
+const { config } = require('./../database/config');
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'sabino',
-  password: 'admin123',
-  database: 'my_store',
-});
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+
+const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const pool = new Pool({ connectionString: URL });
+// const pool = new Pool({
+//   host: 'localhost',
+//   port: 5432,
+//   user: 'sabino',
+//   password: 'admin123',
+//   database: 'my_store',
+// });
 module.exports = pool;
